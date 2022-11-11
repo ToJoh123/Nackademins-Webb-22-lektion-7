95417f94f8c68dc34037ceac796a4accd332d385
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2022-11-11 13:53:24 +0100

    added products

---
+++ script.js
@@ -0,0 +1,68 @@
+// Random price from 250 to 4000
+const randomPrice = () => Math.floor(Math.random() * 3750) + 250;
+
+const shoes = [
+    {
+        name: 'Nike Air Force 1',
+        price: randomPrice(),
+        unit: "$",
+        image: "https://picsum.photos/200",
+        description: 'The Nike Air Force 1 is a classic basketball shoe that has been around since 1982. It was the first basketball shoe to feature Nike Air technology, revolutionizing the game and sneaker culture. The shoe has been re-released in many different colorways and styles, and is still a popular choice for basketball players and sneakerheads alike.'
+    },
+    {
+        name: 'Nike Air Force 2',
+        price: randomPrice(),
+        unit: "$",
+        image: 'https://picsum.photos/201',
+        description: 'The Nike Air Force 1 is a classic basketball shoe that has been around since 1982. It was the first basketball shoe to feature Nike Air technology, revolutionizing the game and sneaker culture. The shoe has been re-released in many different colorways and styles, and is still a popular choice for basketball players and sneakerheads alike.'
+    },
+    {
+        name: 'Nike Air Force 3',
+        price: randomPrice(),
+        unit: "$",
+        image: 'https://picsum.photos/202',
+        description: 'The Nike Air Force 1 is a classic basketball shoe that has been around since 1982. It was the first basketball shoe to feature Nike Air technology, revolutionizing the game and sneaker culture. The shoe has been re-released in many different colorways and styles, and is still a popular choice for basketball players and sneakerheads alike.'
+    },
+    {
+        name: 'Nike Air Force 4',
+        price: randomPrice(),
+        unit: "$",
+        image: 'https://picsum.photos/203',
+        description: 'The Nike Air Force 1 is a classic basketball shoe that has been around since 1982. It was the first basketball shoe to feature Nike Air technology, revolutionizing the game and sneaker culture. The shoe has been re-released in many different colorways and styles, and is still a popular choice for basketball players and sneakerheads alike.'
+    },
+    {
+        name: 'Nike Air Force 5',
+        price: randomPrice(),
+        unit: "$",
+        image: 'https://picsum.photos/204',
+        description: 'The Nike Air Force 1 is a classic basketball shoe that has been around since 1982. It was the first basketball shoe to feature Nike Air technology, revolutionizing the game and sneaker culture. The shoe has been re-released in many different colorways and styles, and is still a popular choice for basketball players and sneakerheads alike.'
+    },
+    {
+        name: 'Nike Air Force 6',
+        price: randomPrice(),
+        unit: "$",
+        image: 'https://picsum.photos/205',
+        description: 'The Nike Air Force 1 is a classic basketball shoe that has been around since 1982. It was the first basketball shoe to feature Nike Air technology, revolutionizing the game and sneaker culture. The shoe has been re-released in many different colorways and styles, and is still a popular choice for basketball players and sneakerheads alike.'
+    },
+    {
+        name: 'Nike Air Force 7',
+        price: randomPrice(),
+        unit: "$",
+        image: 'https://picsum.photos/206',
+        description: 'The Nike Air Force 1 is a classic basketball shoe that has been around since 1982. It was the first basketball shoe to feature Nike Air technology, revolutionizing the game and sneaker culture. The shoe has been re-released in many different colorways and styles, and is still a popular choice for basketball players and sneakerheads alike.'
+    },
+    {
+        name: 'Nike Air Force 8',
+        price: randomPrice(),
+        unit: "$",
+        image: 'https://picsum.photos/207',
+        description: 'The Nike Air Force 1 is a classic basketball shoe that has been around since 1982. It was the first basketball shoe to feature Nike Air technology, revolutionizing the game and sneaker culture. The shoe has been re-released in many different colorways and styles, and is still a popular choice for basketball players and sneakerheads alike.'
+    },
+    {
+        name: 'Nike Air Force 9',
+        price: randomPrice(),
+        unit: "$",
+        image: 'https://picsum.photos/208',
+        description: 'The Nike Air Force 1 is a classic basketball shoe that has been around since 1982. It was the first basketball shoe to feature Nike Air technology, revolutionizing the game and sneaker culture. The shoe has been re-released in many different colorways and styles, and is still a popular choice for basketball players and sneakerheads alike.'
+    },
+]
